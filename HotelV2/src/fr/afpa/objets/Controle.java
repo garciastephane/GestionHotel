4220fcac0c4326fc0f1acc6009be6800dff03cf2
Marine <zimmer.marine@gmail.com>
2019-10-20 17:51:54 +0200

    MZ_controle_date_exception

--- HotelV2/src/fr/afpa/objets/Controle.java
+++ HotelV2/src/fr/afpa/objets/Controle.java
@@ -1,15 +1,17 @@
 package fr.afpa.objets;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Controle {
 
 	/**
-	 * methode qui retourne true si la chaine de caractère est de type numérique et
+	 * methode qui retourne true si la chaine de caractères est de type numérique et
 	 * de la bonne taille
 	 * 
-	 * @param chaine : chaine de caractère à tester
-	 * @param taille : taille que la chaine de caractère doit avoir
+	 * @param chaine : chaine de caractères à tester
+	 * @param taille : taille que la chaine de caractères doit avoir
 	 * @return true si la chaine est numérique et de la bonne taille, false sinon
 	 */
 	public static boolean isNumerique(String chaine, int taille) {
@@ -26,12 +28,12 @@ public class Controle {
 	}
 
 	/**
-	 * methode qui retourne true si la chaine de caractère est de type
-	 * alpha-numérique(commence par 2 majuscules puis des chiffres) et de la bonne
+	 * methode qui retourne true si la chaine de caractères est de type
+	 * alpha-numérique(commence par 2 majuscules (GH puis des chiffres) et de la bonne
 	 * taille
 	 * 
 	 * @param chaine : chaine de caractère à tester
-	 * @param taille : taille que la chaine de caractère doit avoir
+	 * @param taille : taille que la chaine de caractères doit avoir
 	 * @return true si la chaine est alpha-numérique et de la bonne taille, false
 	 *         sinon
 	 */
@@ -47,32 +49,12 @@ public class Controle {
 		return true;
 	}
 
-	/**
-	 * methode qui verifie si la chaine de caractère est un double
-	 * 
-	 * @param decimal : chaine de caractè à tester
-	 * @return true si decimal est du format double
-	 */
-	public static boolean isDouble(String decimal) {
-		String temp;
-		if (decimal.length() > 0 && decimal.charAt(0) == '-') {
-			decimal = decimal.substring(1);
-		}
-		temp = decimal.replaceAll("\\.", "");
-		if (temp.length() > 0 && temp.length() + 1 >= decimal.length()) {
-			if (!isNumerique(temp, temp.length())) {
-				return false;
-			}
-		} else {
-			return false;
-		}
-		return true;
-	}
+
 
 	/**
 	 * methode qui verifie si la réponse est oui ou non
 	 * 
-	 * @param reponse : chaine de caractère à vérifier
+	 * @param reponse : chaine de caractères à vérifier
 	 * @return true si la reponse est 'o' ou 'n', false sinon
 	 */
 	public static boolean isOuiOuNon(String reponse) {
@@ -83,11 +65,11 @@ public class Controle {
 	}
 
 	/**
-	 * methode qui verifie si la chaine de caractere et non vide (les espaces sont
+	 * methode qui verifie si la chaine de caracteres et non vide (les espaces sont
 	 * concidéré comme vide)
 	 * 
-	 * @param chaine chaine de caractère à vérifier
-	 * @return true si la chaine de caractère est non vide et n'est pas constitué
+	 * @param chaine chaine de caractères à vérifier
+	 * @return true si la chaine de caractères est non vide et n'est pas constituée
 	 *         que des espaces, false sinon
 	 */
 	public static boolean isNonVide(String chaine) {
@@ -165,33 +147,19 @@ public class Controle {
 	
 
 	/**
-	 * methode qui retourne si la chaine de caractère est une date valide
+	 * methode qui retourne si la chaine de caractères est une date valide
 	 * 
 	 * @param date chaine de caractère à tester (jj/mm/aaaa)
 	 * @return true si la chaine est une date valide, false sinon
 	 */
 	public static boolean isDateValide(String date) {
 
-		if (date.length() != 10 || date.charAt(2) != '/' || date.charAt(5) != '/'
-				|| !isNumerique(date.replace('/', '0'), date.length())) {
-			return false;
-		}
-
-		boolean bissextile;
-		int jour = Integer.parseInt("" + date.charAt(0) + date.charAt(1));
-		int mois = Integer.parseInt("" + date.charAt(3) + date.charAt(4));
-		int annee = Integer.parseInt("" + date.charAt(6) + date.charAt(7) + date.charAt(8) + date.charAt(9));
-
-		bissextile = ((annee % 4 == 0 && annee % 100 != 0) || (annee % 4 == 0 && annee % 400 == 0));
-
-		if ((jour < 1 || jour > 31 || mois > 12 || mois < 1)
-				|| ((mois == 4 || mois == 6 || mois == 9 || mois == 11) && jour > 30)
-				|| (mois == 2 && jour > 28 && !bissextile) || (mois == 2 && jour > 29 && bissextile)) {
-
-			return false;
-
-		}
-		return true;
+		try {
+            LocalDate date2=LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        }catch(Exception e) {
+            return false;
+        }
+        return true;
 
 	}
 
